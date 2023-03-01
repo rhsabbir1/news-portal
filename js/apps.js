464463e05b02e903d351f1b159628c4d3403f581
rhsabbir1 <rhsabbir090@gmai.com>
2023-03-01 12:27:36 +0600

    get single news by id

--- js/apps.js
+++ js/apps.js
@@ -7,10 +7,27 @@ const allNewsCatagoris = () => {
 const displayAllcatagorics = (data) => {
     const catagoryContainer = document.getElementById('catagory-container');
     data.forEach(catagory => {
+        // console.log(catagory)
         catagoryContainer.innerHTML += `
-        <a class="text-decoration-none fs-5" href="">${catagory.category_name} </a>
+        <p>
+           <a class="text-decoration-none fs-5" onclick=" getAllNewsById('${catagory.category_id}')">${catagory.category_name} </a>
+        </p>
         `;
     });
+}
+
+
+const getAllNewsById = (category_id) =>{
+    const URL =` https://openapi.programming-hero.com/api/news/category/${category_id}`;
+    fetch(URL)
+    .then(res => res.json())
+    .then(data => showGetNews(data.data))
+}
+
+const showGetNews =(data) =>{
+    data.forEach(singleNews =>{
+        console.log(singleNews)
+    })
 }
 
 allNewsCatagoris()
