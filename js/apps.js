c8d8ad009076c6991288d9692f15887b97cd2b95
rhsabbir1 <rhsabbir090@gmai.com>
2023-03-01 12:42:40 +0600

    catagory length and name show alert

--- js/apps.js
+++ js/apps.js
@@ -10,23 +10,25 @@ const displayAllcatagorics = (data) => {
         // console.log(catagory)
         catagoryContainer.innerHTML += `
         <p>
-           <a class="text-decoration-none fs-5" onclick=" getAllNewsById('${catagory.category_id}')">${catagory.category_name} </a>
+           <a class="text-decoration-none fs-5" onclick=" getAllNewsById('${catagory.category_id}' , '${catagory.category_name}')">${catagory.category_name} </a>
         </p>
         `;
     });
 }
 
 
-const getAllNewsById = (category_id) =>{
+const getAllNewsById = (category_id , name) =>{
     const URL =` https://openapi.programming-hero.com/api/news/category/${category_id}`;
     fetch(URL)
     .then(res => res.json())
-    .then(data => showGetNews(data.data))
+    .then(data => showGetNews(data.data , name))
 }
 
-const showGetNews =(data) =>{
+const showGetNews =(data , name) =>{
+    document.getElementById('catagory-count').innerText = data.length;
+    document.getElementById('catagory-name').innerText = name;
     data.forEach(singleNews =>{
-        console.log(singleNews)
+        
     })
 }
 
