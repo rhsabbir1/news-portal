6bc0c48d60615f1b8ca99f00dd7dd1877e590970
rhsabbir1 <rhsabbir090@gmai.com>
2023-03-01 11:40:55 +0600

    get all news

--- js/apps.js
+++ js/apps.js
@@ -1,1 +1,20 @@
-console.log(5)
+const allNewsCatagoris = () => {
+    fetch('https://openapi.programming-hero.com/api/news/categories')
+        .then(res => res.json())
+        .then(data => displayAllcatagorics(data.data.news_category))
+}
+
+const displayAllcatagorics = (data) => {
+
+    data.forEach(catagory => {
+        
+    });
+}
+
+allNewsCatagoris()
+
+
+
+// git add .
+// git commmit -m"get catagories"
+// git push
